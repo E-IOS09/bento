e811663798be0deeccc3a7377d917494b742b8f3
E-IOS09 <mangaleshita@gmail.com>
2024-11-02 18:45:05 +0000

    Build the whole portfolio with projects and contact pages . Remove the input.css and used inbuild tailwind in html

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleOverlay() {
-    const overlay = document.getElementById('overlay');
-    overlay.classList.toggle('hidden');
-}
-
-// Adding click event listeners to navbar links to trigger overlay
-document.querySelectorAll('.navbar-link').forEach(link => {
-    link.addEventListener('click', toggleOverlay);
-});
